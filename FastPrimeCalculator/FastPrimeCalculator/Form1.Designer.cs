109b31861c20ae68f6cb9a6449f630ae234f6ce7
Tom Logue <odinbolt@hotmail.com>
2016-07-18 03:00:37 -0700

    added multi-threaded prime number table lookup

--- FastPrimeCalculator/FastPrimeCalculator/Form1.Designer.cs
+++ FastPrimeCalculator/FastPrimeCalculator/Form1.Designer.cs
@@ -16,6 +16,7 @@
             if (disposing && (components != null))
             {
                 components.Dispose();
+                calculationTimer.Dispose();
             }
             base.Dispose(disposing);
         }
--- FastPrimeCalculator/FastPrimeCalculator/Form1.cs
+++ FastPrimeCalculator/FastPrimeCalculator/Form1.cs
@@ -16,11 +16,13 @@ namespace FastPrimeCalculator
         // Time remaining to perform prime number calculations
         UInt32 timeRemaining = 0;
 
-        // the 
+        // the timer limiting how long we can calculate primes
         private System.Threading.Timer calculationTimer = null;
 
         // we'll perform the actual calculation on a thread separate from the UI thread
         private Thread primeNumberCalculationThread = null;
+
+        private bool abortMainCalculationThread = false;
 
         /// <summary>
         /// Constructor for the basic form UI supporting this application
@@ -77,6 +79,9 @@ namespace FastPrimeCalculator
             AbortButton.Enabled = true;
             StartButton.Enabled = false;
 
+            abortMainCalculationThread = false;
+            primeNumbers = new List<ulong>();
+
             UInt32.TryParse(CalculationTimeout.Text, out timeRemaining);
 
             // if the provided timeout is not a positive number, display an error and return
@@ -131,49 +136,132 @@ namespace FastPrimeCalculator
             int i = 0;
 
             // save a prime numbers list to optimize calculations
-            List<UInt64> primeNumbers = new List<ulong>();
+            //List<UInt64> primeNumbers = new List<ulong>();
+
+            Thread[] workerThreads = new Thread[Environment.ProcessorCount];
 
             // Calculation will run 'forever' until instructed to stop via timer or user interrupt.
-            // This first attempt is a simple brute-force, single-threaded calculation but we can
-            // immediately leverage it into a unit test once boilerplate prototyping is completed
-            // to ensure optimizations still produce a valid calculation of prime numbers
+            // this method utilizes a prime number lookup table to reduce the number of calculations required.
+            // it also performs parallel processing on the lookup table above a certain threshold.
+            // the threshold value simply reduces the multi-thread overhead when the table size
+            // is small and single-threaded may be more efficient.
             while (true)
             {
-                //for (i = 2; i < valueTestedForPrime; i++)
-                //{
-                //    if (valueTestedForPrime % i == 0)
-                //    {
-                //        // Value under test is divisible by a smaller number, therefore NOT prime
-                //        break;
-                //    }
-                //}
-
-                for(i = 0; i<primeNumbers.Count; i++)
+                // work on single thread until it's efficient to move to parallel processing
+                if (primeNumbers.Count < 20000)
                 {
-                    if (valueTestedForPrime % primeNumbers[i] == 0)
+                    for (i = 0; i < primeNumbers.Count; i++)
                     {
-                        // Value under test is divisible by a smaller number, therefore NOT prime
-                        break;
+                        if (valueTestedForPrime % primeNumbers[i] == 0)
+                        {
+                            // Value under test is divisible by a smaller number, therefore NOT prime
+                            break;
+                        }
+                    }
+
+                    if (abortMainCalculationThread)
+                    {
+                        // if we've aborted, don't add any more primes
+                        return;
+                    }
+
+                    if (primeNumbers.Count == i)
+                    {
+                        // PRIME!!!
+                        primeNumbers.Add(valueTestedForPrime);
+                        BeginInvoke(new Action<UInt64, int>(UpdatePrimeNumber), new object[2] { valueTestedForPrime, primeNumbers.Count });
                     }
                 }
-
-                if(primeNumbers.Count == i)
+                else
                 {
-                    // PRIME!!!
-                    primeNumbers.Add(valueTestedForPrime);
-                    BeginInvoke(new Action<UInt64,int>(UpdatePrimeNumber), new object[2] { valueTestedForPrime, primeNumbers.Count });
+                    int sectionSize = primeNumbers.Count / workerThreads.Length;
+                    for (i = 0; i < workerThreads.Length - 1; i++)
+                    {
+                        workerThreads[i] = new Thread(new ParameterizedThreadStart(PrimeCalculationWorker));
+                        object args = new object[4] { valueTestedForPrime, (i * sectionSize), (i * sectionSize + sectionSize - 1), i };
+                        workerThreads[i].Start(args);
+                    }
+                    workerThreads[i] = new Thread(new ParameterizedThreadStart(PrimeCalculationWorker));
+                    object argsEnd = new object[4] { valueTestedForPrime, (i * sectionSize), primeNumbers.Count - 1, i };
+                    workerThreads[i].Start(argsEnd);
+
+                    for (i = 0; i < workerThreads.Length; i++)
+                    {
+                        workerThreads[i].Join();
+                    }
+
+                    if (abortMainCalculationThread)
+                    {
+                        // if we've aborted, don't add any more primes
+                        return;
+                    }
+
+                    if (workerThreadAppearsPrime.All(x => x))
+                    {
+                        // PRIME!!!
+                        primeNumbers.Add(valueTestedForPrime);
+                        BeginInvoke(new Action<UInt64, int>(UpdatePrimeNumber), new object[2] { valueTestedForPrime, primeNumbers.Count });
+                    }
+
+                    // reset the thread results to assume not prime (aka false)
+                    for (int k=0; k<workerThreadAppearsPrime.Length; k++)
+                    {
+                        workerThreadAppearsPrime[k] = false;
+                    }
+                    abortWorkerThread = false;
                 }
 
                 valueTestedForPrime++;
             }
         }
 
+        // save a prime numbers list to optimize calculations
+        List<UInt64> primeNumbers = new List<ulong>();
+
+        // boolean event to indicate we should abort the worker thread
+        volatile bool abortWorkerThread = false;
+
+        bool[] workerThreadAppearsPrime = new bool[Environment.ProcessorCount];
+
+        /// <summary>
+        /// Worker thread to calculate a portion of the prime numbers table
+        /// <param name="args">arg1 is the value under test, arg2 is the starting position, arg3 is the ending position in the prime list comparison, arg4 is the thread number</param>
+        /// </summary>
+        void PrimeCalculationWorker(object args)
+        {
+            UInt64 valueTestedForPrime = (UInt64)((Array)args).GetValue(0);
+            int start = (int)((Array)args).GetValue(1);
+            int finish = (int)((Array)args).GetValue(2);
+            int threadNumber = (int)((Array)args).GetValue(3);
+            int i = 0;
+
+            for(i=start; i <= finish; i++)
+            {
+                if (abortWorkerThread) break;
+
+                if (valueTestedForPrime % primeNumbers[i] == 0)
+                {
+                    // Value under test is divisible by a smaller number, therefore NOT prime
+                    abortWorkerThread = true;
+                    break;
+                }
+            }
+            if(i > finish)
+            {
+                // appears prime in this section
+                workerThreadAppearsPrime[threadNumber] = true;
+            }
+        }
+
+
         /// <summary>
         /// This method will finish the prime number calculation cycle and reset the UI buttons.
         /// </summary>
         protected void FinishCalculation()
         {
-            primeNumberCalculationThread.Abort();
+            abortMainCalculationThread = true;
+            abortWorkerThread = true;
+
             calculationTimer.Change(Timeout.Infinite, Timeout.Infinite);
             BeginInvoke(new Action(ResetButtonState), null);
         }
